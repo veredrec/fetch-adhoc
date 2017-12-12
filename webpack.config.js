1117bd6cac982d54fd5ea9394e98d7c1b2e22c79
Vered Rekanati <veredrec@gmail.com>
2017-12-12 19:49:21 +0200

    set up all files and folders for project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+module.exports = {
+  entry: {
+    main: ['babel-polyfill', './index.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        babelrc: false,
+        query: {
+          presets: ['env'],
+          compact: false
+        }
+      }
+    ]
+  },
+  output: {
+    path: 'dist/',
+    filename: '[name].js',
+    libraryTarget: 'var',
+    library: 'RecordManager'
+  },
+  plugins: [new webpack.IgnorePlugin(/vertx/)]
+};
